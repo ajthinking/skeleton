9efa2e8f674b86c0f301e79ce48b2247501e255f
Anders Jurisoo <jurisoo@hotmail.com>
2019-05-09 19:35:02 +0800

    Started work on packagifying it

--- Skeleton/routes/routes.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-Route::view('/skeleton', 'skeleton::spa');
-
-Route::prefix('skeleton/api')->group(function () {
-    Route::get('/templates', '\Skeleton\Controllers\SkeletonAPIController@templates');
-    Route::post('/build',    '\Skeleton\Controllers\SkeletonAPIController@build');
-});
